1a50599523e7f351d1b5c0542ea9076d83ca70ea
Marius Mircea <marius.mircea@haufe.com>
2021-08-22 23:03:17 +0300

    [client] Add simple Home page

---
+++ client/src/components/pages/home/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <div>
+            <p>
+                This is a work in progress page. Check search + products + product page :)
+            </p>
+        </div>
+    )
+}
+
+export default HomePage
