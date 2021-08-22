a247a188cfa5549e2055ca5fff9ca02fa8214997
Marius Mircea <marius.mircea@haufe.com>
2021-08-22 23:00:52 +0300

    [client] Enhance ProductModel

--- client/src/models/ProductModel.ts
+++ client/src/models/ProductModel.ts
@@ -2,15 +2,27 @@ class ProductModel {
     id: string
     description: string
     title: string
-    imageUrl: string
+    link: string
+    imageLink: string
     price: number
+    availability: string
+    currency: string
+    brand: string
+    condition: string
+    shipping: string
 
     constructor(data: ProductModel) {
         this.id = data.id
         this.description = data.description
         this.title = data.title
-        this.imageUrl = data.imageUrl
+        this.link = data.link
+        this.imageLink = data.imageLink
         this.price = data.price
+        this.availability = data.availability
+        this.currency = data.currency
+        this.brand = data.brand
+        this.condition = data.condition
+        this.shipping = data.shipping
     }
 }
 
