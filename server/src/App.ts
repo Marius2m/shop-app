ac17057259895eb66b0ef38dbd947fb371865b1a
Marius Mircea <marius.mircea@haufe.com>
2021-08-22 19:54:57 +0300

    [server] added generic https status code errors

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import { ConnectToDB } from './middlewares/DBConnection'
+import { logger } from './config/logger'
+import { NotFoundError } from './utils/Errors'
 
 const app = express()
 
@@ -12,5 +14,18 @@ app.use(ConnectToDB({
 
 // Paths
 app.use('/health', (req, res, next) => res.json({ ams: ':)' }))
+
+// Session End
+// app.use(CloseDBConnection)
+app.use((err, req, res, next) => {
+	if (err) logger.error('Err', err)
+
+	if (err instanceof NotFoundError) {
+		res.status(404).json({ message: err.message })
+	} else {
+		res.status(500).json({ message: err.message })
+	}
+	next()
+})
 
 export default app
---
+++ server/src/utils/Errors.ts
@@ -0,0 +1,3 @@
+export class NotFoundError extends Error {}
+export class UnprocessableEntityError extends Error {}
+export class ServerError extends Error {}
